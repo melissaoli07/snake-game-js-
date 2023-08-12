65a7adf61833b6f81f0eea9fec77bb408a2e14e6
melissaoli07 <melissaoliveiraa11@gmail.com>
2023-08-12 13:29:53 -0300

    edicao

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+const size = 30
+
+const snake = [{ x: 200, y: 200}]
+
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd"
+    ctx.fillRect(snake[0].x, snale[0].y, size, size)
+}
+
+drawSnale()
