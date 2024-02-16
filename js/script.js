a8a3f9815d7f5d2fbdcb12df68c5c3dcabc0bfa6
melissaoli07 <melissaoliveiraa11@gmail.com>
2024-02-15 22:31:20 -0300

    15_02

--- js/script.js
+++ js/script.js
@@ -3,11 +3,58 @@ const ctx = canvas.getContext("2d")
 
 const size = 30
 
-const snake = [{ x: 200, y: 200}]
+const snake = [
+    { x: 200, y: 200},
+    { x: 230, y: 200}
+]
+
+let direction 
 
 const drawSnake = () => {
     ctx.fillStyle = "#ddd"
-    ctx.fillRect(snake[0].x, snake[0].y, size, size)
+    
+    snake.forEach((position, index) => {
+    if(index ==snake.length -1) {
+        ctx.fillStyle = "white"
+    }
+
+    ctx.fillRect(position.x, position.y, size, size)
+    })
 }
 
-drawSnake()
+const moveSnake = () => {
+    if (!direction) return
+
+    const head = snake[snake.length -1]
+
+    if (direction == "rigth") {
+        snake.push({x: head.x + size, y: head.y})
+    }
+
+    if (direction == "left") {
+        snake.push({x: head.x - size, y: head.y})
+    }
+
+    if (direction == "down") {
+        snake.push({x: head.x, y: head.y + size})
+    }
+
+    if (direction == "up") {
+        snake.push({x: head.x, y: head.y - size})
+    }
+
+    snake.shift()
+}
+
+const gameLoop = () => {
+    ctx.clearRect(0, 0, 600, 600)
+
+    moveSnake()
+    drawSnake()
+
+    setTimeout(() => (
+        gameLoop()
+    ), 300)
+}
+
+gameLoop()
